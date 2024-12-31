aa24acaec1f2a7d0cc2fe09f41640c827eaa9b75
Gilles <git@example.com>
2024-12-31 20:09:57 +0100

    First import of spring batch beginner project

---
+++ beginnerbatch/src/main/java/com/example/spring/beginnerbatch/BeginnerbatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.beginnerbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeginnerbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeginnerbatchApplication.class, args);
+	}
+
+}
---
+++ beginnerbatch/src/test/java/com/example/spring/beginnerbatch/BeginnerbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.beginnerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeginnerbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
