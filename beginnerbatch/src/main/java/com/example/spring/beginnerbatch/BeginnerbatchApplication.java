fb78431f2a362a311db6ae7753082f8ba1a8b68f
Gilles <git@example.com>
2025-01-01 20:11:37 +0100

    Implémentation de la lecture du fichier rawsensordata avec log des items lus

--- beginnerbatch/src/main/java/com/example/spring/beginnerbatch/BeginnerbatchApplication.java
+++ beginnerbatch/src/main/java/com/example/spring/beginnerbatch/BeginnerbatchApplication.java
@@ -1,13 +1,13 @@
 package com.example.spring.beginnerbatch;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.batch.core.launch.support.CommandLineJobRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BeginnerbatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeginnerbatchApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		CommandLineJobRunner.main(args);
 	}
 
 }
